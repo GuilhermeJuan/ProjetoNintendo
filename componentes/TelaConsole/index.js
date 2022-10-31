7bc69c0bee756c8c2e3f0d50ebcccfa62aeb449b
d <diogogpontes@gmail.com>
2022-10-31 09:56:55 -0300

    b

--- componentes/TelaConsole/index.js
+++ componentes/TelaConsole/index.js
@@ -3,8 +3,8 @@ import { ScrollView, View, Image, Text } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
 
 import styles from './styles';
-import imgConsole1 from '../../assets/xboxS.png';
-import imgConsole2 from '../../assets/xboxX.png';
+import imgConsole1 from '../../assets/ps4.jpg';
+import imgConsole2 from '../../assets/ps5.jpg';
 
 
 export default function TelaConsole() {
@@ -13,16 +13,16 @@ const Navigation = useNavigation();
   <ScrollView style={ styles.container}>
   <View style={ styles.box }>
     <Image source={ imgConsole1 } style={ styles.img } />
-    <Text style={ styles.titulo }>XBOX SERIES S</Text>
+    <Text style={ styles.titulo }>PLAYSTATION 4</Text>
     <Text style={ styles.subtitulo }>Desempenho de última geração no menor Xbox de todos os tempos.</Text>
     <Text style={ styles.paragrafo }>O Series S é pra quem busca jogos de nova geração gastando pouco. é o console mais barato da nova geração. Ele é o equilibrio entre gráfico e performance</Text>
   </View>
   
   <View style={ styles.box }>
   <Image source={ imgConsole2 } style={ styles.img } />
-    <Text style={ styles.titulo }>XBOX SERIES X</Text>
-    <Text style={ styles.subtitulo }>O Xbox mais rápido e poderoso de todos os tempos.</Text>
-    <Text style={ styles.paragrafo }>O Series X é pra quem quer poder absoluto. O console mais poderoso do mercado, excelência em gráficos e performance.</Text>
+    <Text style={ styles.titulo }>PLAYSTATION 5</Text>
+    <Text style={ styles.subtitulo }>O PlayStation 5 é o mais novo modelo lançado pela sony.</Text>
+    <Text style={ styles.paragrafo }>O PlayStation 5 (PS5) é o novo console que chega sete anos após o lançamento do seu antecessor PlayStation 4 (PS4). O videogame chega ao Brasil no dia 19 de novembro pelo preço de R$ 4.499, na versão digital, e R$ 4.999, na edição com leitor Blu-Ray Ultra HD. Suas especificações foram divulgadas em março de 2020 durante uma transmissão online no PlayStation Blog, com comando de Mark Cerny, arquiteto-chefe responsável do aparelho. No streaming ao vivo, a marca divulgou a ficha técnica do concorrente do Xbox Series X. O destaque é a CPU AMD Zen 2, de 3,5 GHz, e o SSD personalizado de 825 GB superrápido que, de acordo com a fabricante, pode torná-lo o videogame "mais rápido do mundo". O processador gráfico de 10,28 Teraflops, em 2,23 GHz, indica uma capacidade de geração de poder visual menor do que seu principal concorrente da Microsoft, que vem equipado com 12 TFLOPs. Outra característica que chama a atenção é o Tempest Engine, sistema de áudio 3D que promete fidelidade e qualidade de som com alto nível de detalhes. </Text>
   </View>
 </ScrollView>
   );
